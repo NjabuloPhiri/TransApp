44ed0ac84900b0bbaa868f6b226958532ce5cdc1
Njabulo <njabulophiri203@gmail.com>
2018-11-07 17:34:28 +0200

    Assignment3

---
+++ Assignment3/service-worker.js
@@ -0,0 +1,16 @@
+ var PRECACHE_URLS = [
+    '/',
+    //html file
+    'index.html',
+    //html manifest pointer
+    'index.html?homescreen=1',
+    '?homescreen=1',
+    //css file
+    'bulma.min.css',
+    //javascript
+    'index.js'
+    //manifest file
+    'manifest.json',
+    //assets or images
+    'public-transport-icon.png'
+ ];
